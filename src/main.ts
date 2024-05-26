5830dd3d404e7a0dd42e75e819a6c694a3962272
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2024-05-26 14:22:21 +0700

    test(sentry): skip tracePropagationTargets

--- src/main.ts
+++ src/main.ts
@@ -14,11 +14,11 @@ Sentry.init({
   tracesSampleRate: 1.0, //  Capture 100% of the transactions
   // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
   // https://attilacsanyi.github.io/myapp/
-  tracePropagationTargets: [
-    'localhost',
-    /^https:\/\/yourserver\.io\/api/,
-    'https://attilacsanyi.github.io',
-  ],
+  // tracePropagationTargets: [
+  //   'localhost',
+  //   /^https:\/\/yourserver\.io\/api/,
+  //   'https://attilacsanyi.github.io',
+  // ],
   // Session Replay
   replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
